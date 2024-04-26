38e81276273c8af1c3b3f3316e3ff438af10115f
Kaviya K <94157353+Kaviyakalaiselvam@users.noreply.github.com>
2024-04-26 13:01:17 +0530

    Create multiple.js

---
+++ multiple.js
@@ -0,0 +1,7 @@
+let numbers=[1,2,3];
+let multiplication=[];
+for(let i = 0; i < numbers.length; i++) {
+    if (numbers[i] * 2 )
+    multiplication.push(numbers[i]*2);
+}
+console.log(`Multiple of number is: ${multiplication}`);
